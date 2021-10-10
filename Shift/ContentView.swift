bc7c3e14be503e84d2c5c492e1d0c7f200736576
Piotr Ćwiertnia <32513700+rezt@users.noreply.github.com>
2021-10-10 16:14:35 +0200

    Initial Commit

---
+++ Shift/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Shift
+//
+//  Created by Piotr Ćwiertnia on 10/10/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Shift/ShiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShiftApp.swift
+//  Shift
+//
+//  Created by Piotr Ćwiertnia on 10/10/2021.
+//
+
+import SwiftUI
+
+@main
+struct ShiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
