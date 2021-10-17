d397fd10c5cc165a5405120217acc2f933b95dbf
Piotr Ćwiertnia <32513700+rezt@users.noreply.github.com>
2021-10-17 15:22:44 +0200

    add CustomViewModifier

--- Shift/View/LoginView.swift
+++ Shift/View/LoginView.swift
@@ -15,22 +15,62 @@ struct LoginView: View {
     
     var body: some View {
         NavigationView {
-        VStack {
-            NavigationLink(destination: MainView(), isActive: $auth.isLoggedIn) {EmptyView()}
-            Spacer()
-            Text("Swift Shift").bold()
-            Spacer()
-            TextField("Login", text: $email).textContentType(.emailAddress).keyboardType(.emailAddress).autocapitalization(.none).disableAutocorrection(true)
-            SecureField("Password", text: $password)
-            Button(action: {auth.login(withEmail: email, withPassword: password)}) {
-                Text("Log in")
-            }
-            Spacer()
-            
+            ZStack{
+                Color.black
+                    .ignoresSafeArea()
+                VStack {
+                    NavigationLink(destination: MainView(), isActive: $auth.isLoggedIn) {EmptyView()}
+                    Spacer()
+                    Text("Swift Shift")
+                        .bold()
+                        .font(.largeTitle)
+                        .foregroundColor(.white)
+                    Spacer()
+                    Text("Login:").bold().font(.largeTitle).foregroundColor(.white)
+                        TextField("", text: $email)
+                            .textContentType(.emailAddress)
+                            .keyboardType(.emailAddress)
+                            .autocapitalization(.none)
+                            .disableAutocorrection(true)
+                            .modifier(CustomViewModifier(roundedCornes: 5, startColor: .purple, endColor: .indigo, textColor: .white))
+                            .padding()
+                        Text("Password:").bold().font(.largeTitle).foregroundColor(.white)
+                        SecureField("PASSWORD", text: $password)
+                            .modifier(CustomViewModifier(roundedCornes: 5, startColor: .purple, endColor: .indigo, textColor: .white))
+                            .padding()
+                    
+                    
+                    Button(action: {auth.login(withEmail: email, withPassword: password)}) {
+                        Text("Log in").font(.title)
+                    }
+                    Spacer()
+                }
+            }.navigationTitle("Login")
         }
-        }.navigationTitle("Login")
+        
     }
 }
+
+struct CustomViewModifier: ViewModifier {
+    var roundedCornes: CGFloat
+    var startColor: Color
+    var endColor: Color
+    var textColor: Color
+    
+    func body(content: Content) -> some View {
+        content
+            .padding()
+            .background(LinearGradient(gradient: Gradient(colors: [startColor, endColor]), startPoint: .topLeading, endPoint: .bottomTrailing))
+            .cornerRadius(roundedCornes)
+            .padding(5)
+            .foregroundColor(textColor)
+            .overlay(RoundedRectangle(cornerRadius: roundedCornes)
+                        .stroke(LinearGradient(gradient: Gradient(colors: [startColor, endColor]), startPoint: .topLeading, endPoint: .bottomTrailing), lineWidth: 5))
+        
+            .shadow(radius: 10)
+    }
+}
+
 
 
 struct LoginView_Previews: PreviewProvider {
